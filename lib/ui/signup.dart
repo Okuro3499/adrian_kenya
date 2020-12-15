ae27fac3bab064265d600fbc75b12ce1165cd482
Gideon Okuro <gideonollonde@gmail.com>
2020-12-15 15:54:13 +0300

    create signup page

---
+++ lib/ui/signup.dart
@@ -0,0 +1,187 @@
+import 'package:adrian_kenya/widgets/custom_shape.dart';
+import 'package:adrian_kenya/widgets/customappbar.dart';
+import 'package:adrian_kenya/widgets/responsive_ui.dart';
+import 'package:adrian_kenya/widgets/textformfield.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool checkBoxValue = false;
+  double _height;
+  double _width;
+  double _pixelRatio;
+  bool _large;
+  bool _medium;
+
+  @override
+  Widget build(BuildContext context) {
+
+    _height = MediaQuery.of(context).size.height;
+    _width = MediaQuery.of(context).size.width;
+    _pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    _large =  ResponsiveWidget.isScreenLarge(_width, _pixelRatio);
+    _medium =  ResponsiveWidget.isScreenMedium(_width, _pixelRatio);
+
+    return Material(
+      child: Scaffold(
+        body: Container(
+          height: _height,
+          width: _width,
+          margin: EdgeInsets.only(bottom: 5),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Opacity(opacity: 0.88,child: CustomAppBar()),
+                clipShape(),
+                form(),
+                SizedBox(height: _height/35,),
+                button(),
+                signInTextRow(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget clipShape() {
+    return Stack(
+      children: <Widget>[
+        Opacity(
+          opacity: 0.75,
+          child: ClipPath(
+            clipper: CustomShapeClipper(),
+            child: Container(
+              height: _large? _height/8 : (_medium? _height/7 : _height/6.5),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.white, Colors.white],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Opacity(
+          opacity: 0.5,
+          child: ClipPath(
+            clipper: CustomShapeClipper2(),
+            child: Container(
+              height: _large? _height/12 : (_medium? _height/11 : _height/10),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.white, Colors.white],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget form() {
+    return Container(
+      margin: EdgeInsets.only(
+          left:_width/ 12.0,
+          right: _width / 12.0,
+          top: _height / 20.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            userNameTextFormField(),
+            SizedBox(height: _height / 60.0),
+            emailTextFormField(),
+            SizedBox(height: _height / 60.0),
+            passwordTextFormField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget userNameTextFormField() {
+    return CustomTextField(
+      keyboardType: TextInputType.text,
+      icon: Icons.person,
+      hint: "Username",
+    );
+  }
+
+  Widget emailTextFormField() {
+    return CustomTextField(
+      keyboardType: TextInputType.emailAddress,
+      icon: Icons.email,
+      hint: "Email ID",
+    );
+  }
+
+  Widget passwordTextFormField() {
+    return CustomTextField(
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      icon: Icons.lock,
+      hint: "Password",
+    );
+  }
+
+  Widget button() {
+    return RaisedButton(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      onPressed: () {
+        print("Routing to your account");
+      },
+      textColor: Colors.white,
+      padding: EdgeInsets.all(0.0),
+      child: Container(
+        alignment: Alignment.center,
+//        height: _height / 20,
+        width:_large? _width/4 : (_medium? _width/3.75: _width/3.5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          gradient: LinearGradient(
+            colors: <Color>[Colors.blue[900], Colors.pinkAccent],
+          ),
+        ),
+        padding: const EdgeInsets.all(12.0),
+        child: Text('SIGN UP', style: TextStyle(fontSize: _large? 14: (_medium? 12: 10)),),
+      ),
+    );
+  }
+
+  Widget signInTextRow() {
+    return Container(
+      margin: EdgeInsets.only(top: _height / 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Already have an account?",
+            style: TextStyle(fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop(SIGN_IN);
+              print("Routing to Sign up screen");
+            },
+            child: Text(
+              "Sign in",
+              style: TextStyle(
+                  fontWeight: FontWeight.w800, color: Colors.blue[900], fontSize: 19),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
